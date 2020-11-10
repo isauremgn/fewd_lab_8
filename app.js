a98f77a99d9626f25e6d3114afdf00d8ac492381
isauremgn <isauremignon@gmail.com>
2020-11-10 16:33:38 +0000

    ‘commit’

---
+++ app.js
@@ -0,0 +1,71 @@
+var Planets; 
+Planets = ['Mercury','Venus','Hearth','Mars','Jupiter'];
+
+console.log(Planets);
+
+
+var a = 5, b = 'Hello ', c = 'World!', d = 10;
+a + b;
+b + c;
+a + d;
+
+var total = a + b;
+var totall = b + c;
+var totalll = a + d;
+console.log(total);
+console.log(totall);
+console.log(totalll);
+
+var a = 5, b = 10, c = "5";
+var x = a;
+a == c;
+a === c;
+a == x;
+a != b;
+a > b;
+a < b;
+a >= b;
+a <= b;
+a >= c;
+a <= c;
+
+var largenum = 5 == "5";
+var largenum2 = 5 === "5";
+var largenum3 = 5 == 5;
+var largenum4 = 5 != 10;
+var largenum5 = 5 > 10;
+var largenum6 = 5 < 10;
+var largenum7 = 5 >= 10;
+var largenum8 = 5 <= 10;
+var largenum9 = 5 >= "5";
+var largenum10 = 5 <= "5";
+
+
+console.log(largenum);
+console.log(largenum2);
+console.log(largenum3);
+console.log(largenum4);
+console.log(largenum5);
+console.log(largenum6);
+console.log(largenum7);
+console.log(largenum8);
+console.log(largenum9);
+console.log(largenum10);
+
+
+
+var hello = function(){
+    return 'Hello World!';
+};
+
+var result = hello();
+
+console.log(result);
+
+
+var hello = function(){
+    return 'Hi!';
+};
+
+var x = hello();
+console.log(x);
